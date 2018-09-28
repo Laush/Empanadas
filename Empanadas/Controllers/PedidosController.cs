e5b1aeb40a608629ac04fc75065680c41d7bb9c5
Laush <laush_x4@hotmail.com>
2018-09-28 17:20:15 -0300

    se agrego las vistas de login y controlador pedido

---
+++ Empanadas/Controllers/PedidosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Empanadas.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Iniciar()
+        {
+
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+
+            return View();
+        }
+    }
+}
