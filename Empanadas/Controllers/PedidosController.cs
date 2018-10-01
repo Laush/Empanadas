b35e68c5874d51dc19a4f262a233c49cd3eb30bc
Laush <laush_x4@hotmail.com>
2018-10-01 14:50:38 -0300

    creacion de lista de pedidos ingresados

--- Empanadas/Controllers/PedidosController.cs
+++ Empanadas/Controllers/PedidosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Empanadas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,29 @@ namespace Empanadas.Controllers
 {
     public class PedidosController : Controller
     {
-        // GET: Pedidos
-        public ActionResult Iniciar()
-        {
 
-            return View();
-        }
+        PedidoServicio sp = new PedidoServicio();
+        // GET: Pedidos
 
         public ActionResult Listar()
         {
+            List<Pedido> pedidosList = sp.Listar();
+            return View(pedidosList);
+        }
 
+        // metodo que agrega pedido desde cero
+        public ActionResult Iniciar()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Iniciar(Pedido p)
+        {
+            sp.Agregar(p);
+
+            return RedirectToAction("Listar", "Pedidos");
+
         }
     }
 }
---
+++ Empanadas/Models/PedidoServicio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Empanadas.Models
+{
+    public class PedidoServicio
+    {
+
+        // la inicializo y no necesito un constructor, es privada para que no la modifique nadie mas 
+        private static List<Pedido> Lista = new List<Pedido>();
+
+        public void Agregar(Pedido p)
+        {
+
+            Lista.Add(p);
+
+
+        }
+
+        public List<Pedido> Listar()
+        {
+            return Lista;
+
+        }
+//futuro CRUD
+/*
+        public void Eliminar(int id)
+        {
+            Lista.RemoveAll(p => p.idPedido == id);
+        }
+
+
+        public Pedido ObtenerPorId(int id)
+        {
+            Pedido pedidoEncontrado = null;
+
+            foreach (Pedido p in Lista)
+            {
+                if (p.idPedido.Equals(id))
+                {
+                    pedidoEncontrado = p;
+                }
+            }
+
+            return pedidoEncontrado;
+        }
+
+        public void Modificar(Pedido p)
+        {
+
+            Lista.RemoveAll(p1 => p1.idPedido == p.idPedido);
+            Lista.Add(p);
+
+
+
+
+        }*/
+    }
+}
