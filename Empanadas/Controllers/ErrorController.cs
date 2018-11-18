d51193b20d771b09fc5495fa0011bf24f3565945
Laush <laush_x4@hotmail.com>
2018-11-18 16:50:38 -0300

    ErrorController

---
+++ Empanadas/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Empanadas.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Error al procesar la solicitud";
+            return View();
+        }
+
+        public ActionResult NotFound404()
+        {
+            ViewBag.Title = "Error 404 - File not Found";
+            return View("Index");
+        }
+    }
+}
--- Empanadas/Global.asax.cs
+++ Empanadas/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Empanadas.Controllers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,34 @@ namespace Empanadas
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Do whatever you want to do with the error
+
+            //Show the custom error page...
+            Server.ClearError();
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+
+            if ((Context.Server.GetLastError() is HttpException) && ((Context.Server.GetLastError() as HttpException).GetHttpCode() != 404))
+            {
+                routeData.Values["action"] = "Index";
+            }
+            else
+            {
+                // Handle 404 error and response code
+                Response.StatusCode = 404;
+                routeData.Values["action"] = "NotFound404";
+            }
+            Response.TrySkipIisCustomErrors = true; // If you are using IIS7, have this line
+            IController errorsController = new ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new System.Web.Routing.RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
+
+            Response.End();
         }
     }
 }
